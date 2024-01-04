73994db1ee6c686d93215c3b98b1135e189b5525
Shankhadeep Banerjee <30634751+ShankhadeepBanerjee@users.noreply.github.com>
2024-01-04 13:56:00 +0000

    Chapter 12

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    headers: async () => {
+      return [
+        {
+          source: '/:dashboard*', // Define the path pattern this header applies to
+          headers: [
+            { key: 'x-forwarded-host', value: 'https://sdxv6x-3000.csb.app' },
+          ],
+        },
+      ]
+    },
+  };
+  
+  module.exports = nextConfig;
+  
